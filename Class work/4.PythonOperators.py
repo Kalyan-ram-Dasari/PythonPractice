5f94c5b1b28f5052316e3e987b86eabdbe2ea68a
Kalyan-ram-Dasari <kalyannani049@gmail.com>
2025-07-07 14:57:59 +0530

    python Operators file created

---
+++ Class work/4.PythonOperators.py
@@ -0,0 +1,46 @@
+#4.python Operators
+
+#1. Arithmetic Operators
+
+print("Arithmetic Operators" )
+print()
+
+a = 10
+b = 5
+print("Addition :" , a + b)  # output Addition(a+b):15
+print()
+print("Subtraction :" , a - b) #output Subtraction(a-b):5
+print()
+print("Multiplication :" , a * b)  # output Multiplication(a*b):50  
+print()
+print("Division :" , a / b) #output Division(a/b):2.0
+print()
+print("Modulus :" , a % b) #output modulus(a%b):0
+print()
+print("Exponentiation :" , a ** b) #output Exponentiation(a**b):100000
+print()
+print("Floor Division :" , a // b)# output FloorDivision(a//b):2
+
+
+
+#2. Comparison Operators 
+
+print("Comparison Operators")
+print()
+
+print("Equal to :" , a == b) #output Equal to(a==b):False
+print()
+print("Not Equal to :" , a != b) #output Not Equal to(a!=b):True
+print()
+print("Greater than : ",a>b) #output Greater than(a>b):True
+print()
+print("Less than : " , a<b)# outpput Lessthan(a<b):Flase
+print()
+print("Greater than or Equal to : ",a>=b)# output Greater than or Equal to(a>=b):True
+print()
+print("Less than or Equal to : ", a<=b) #output Less than or Equal to(a<=b):False
+
+
+
+
+
